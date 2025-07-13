ade591c0128a024c77e52b4e7631e14353faf4d7
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-07-13 13:23:52 -0500

    feat: :sparkles: CRUD de categorias post terminado

--- src/api.php
+++ src/api.php
@@ -26,6 +26,14 @@ switch($recurso){
                 echo json_encode($response);
                 break;
             case "POST":
+                $data = json_decode(file_get_contents('php://input'), true);
+                $stmt = $pdo->prepare("INSERT INTO categorias(nombre) VALUES(?)");
+                $stmt->execute([
+                    $data['nombre']
+                ]);
+                http_response_code(201);
+                $data['id'] = $pdo->lastInsertId();
+                echo json_encode($data); // Muestra al usuario todo lo que inserto
                 break;
             case "PUT":
                 break;
